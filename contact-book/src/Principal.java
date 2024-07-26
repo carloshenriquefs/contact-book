f61e09bec1352e34896e0b64c6a997349cd7265d
Carlos Henrique <ichfs9108@gmail.com>
2024-07-26 17:10:01 -0300

    create function - editarContato e excluiLinha

--- contact-book/src/Principal.java
+++ contact-book/src/Principal.java
@@ -52,15 +52,61 @@ public class Principal {
 		System.out.println("E-mail..........: " + mn[linha][2]);
 	}
 
+	public static void editarContato(String mn[][], int l) {
+		System.out.println();
+		System.out.println("--------------- EDITE O CONTATO: ");
+		System.out.println("Nome..........: " + mn[l][0]);
+
+		System.out.print("Celular.......: ");
+		mn[l][1] = entrada.next();
+		System.out.print("E-mail........: ");
+		mn[l][2] = entrada.next();
+	}
+
 	public static void listarAgenda(String mn[][]) {
 		System.out.println("--------------- CONTATOS DA AGENDA: ");
 		for (int l = 0; l < 10; l++) {
 			if (mn[l][0] != "") {
 				exibirContato(mn, l);
-				System.out.println("---------------------------------");
+				System.out.println("-----------------------------------");
 			}
 		}
 		System.out.println("-------------------- FIM DA AGENDA: ");
+	}
+
+	public static void excluiLinha(String mn[][], int l) {
+		mn[l][0] = "";
+		mn[l][1] = "";
+		mn[l][2] = "";
+		System.out.println("Contato Excluído");
+	}
+
+	public static int pesquisarContato(String mn[][], String n) {
+		for (int l = 0; l < 10; l++) {
+			if (mn[l][0].equals(n)) {
+				return l;
+			}
+		}
+		return -1;
+	}
+
+	public static void apagarContato(String mn[][], String n) {
+		boolean achou = false;
+
+		int linha = pesquisarContato(mn, n);
+		String opcao;
+		if (linha != -1) {
+			exibirContato(mn, linha);
+			System.out.print("Confirma a exclusao do contato ? [S]im ou [N]ão? ");
+			opcao = entrada.next();
+			if (opcao.equals("s") || opcao.equals("S")) {
+				excluiLinha(mn, linha);
+			} else {
+				System.out.println("Exclusão cancelada!");
+			}
+		} else {
+			System.out.println("Contato não encontrado!");
+		}
 	}
 
 	public static void main(String[] args) {
@@ -87,11 +133,29 @@ public class Principal {
 				break;
 
 			case 2:
-
+				System.out.println("---------------- EDITANDO (PESQUISE O CONTATO): ");
+				System.out.print("Digite o nome...........: ");
+				nome = entrada.next();
+				linha = pesquisarContato(agenda, nome);
+				if (linha == -1) {
+					System.out.println("Contato não Cadastrado!");
+				} else {
+					exibirContato(agenda, linha);
+					editarContato(agenda, linha);
+				}
 				break;
 
 			case 3:
+				System.out.println("--------------- PESQUISE O CONTATO: ");
+				System.out.println("Digite o nome..........: ");
+				nome = entrada.next();
 
+				linha = pesquisarContato(agenda, nome);
+				if (linha == -1) {
+					System.out.println("Contato não Cadastrado!");
+				} else {
+					exibirContato(agenda, linha);
+				}
 				break;
 
 			case 4:
@@ -99,6 +163,11 @@ public class Principal {
 				break;
 
 			case 5:
+
+				System.out.println("-------------- EXCLUINDO (PESQUISE O CONTATO): ");
+				System.out.print("Digite o nome..........: ");
+				nome = entrada.next();
+				apagarContato(agenda, nome);
 
 				break;
 
